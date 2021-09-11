2efe4bbf4ed18eeb914d235b9b2d8bb5f46e3b9f
Norhan Boghdadi <goodnorhan@gmail.com>
2021-09-11 20:52:03 +0200

    Add PokemontableView to list pokemons

--- Pokemon-Rawfish/ViewController.swift
+++ Pokemon-Rawfish/ViewController.swift
@@ -10,13 +10,53 @@ import UIKit
 class ViewController: UIViewController {
 
     var pokemonsTableView: UITableView!
+    var pokemonElements = [PokemonElements]()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        
+        
+        pokemonsTableView = UITableView()
+        pokemonsTableView.translatesAutoresizingMaskIntoConstraints = false
+//        pokemonsTableView.dataSource = self
+//        pokemonsTableView.delegate = self
+//        pokemonsTableView.register(<#T##nib: UINib?##UINib?#>, forCellReuseIdentifier: <#T##String#>)
+        pokemonsTableView.backgroundColor = .white
+        
+        pokemonsTableView.reloadData()
+        
+        view.addSubview(pokemonsTableView)
+        
+        setupConstraints()
+        
+    }
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            pokemonsTableView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pokemonsTableView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            pokemonsTableView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.75),
+            pokemonsTableView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8)
+        
+        ])
+        
+    }
+
+
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemonElements.count
     }
     
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+extension ViewController: UITableViewDelegate {
+    
 }
 

136525d56a48aefc216ef9eec3e45b62f3681947
Norhan Boghdadi <goodnorhan@gmail.com>
2021-09-11 20:11:05 +0200

    add Pokemon Model file

--- Pokemon-Rawfish/ViewController.swift
+++ Pokemon-Rawfish/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pokemonsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
